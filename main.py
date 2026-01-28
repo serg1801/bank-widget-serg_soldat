4bef26c9942675e27df0592d17957b8b26cfed51
Soldatov Sergei <serg1801soldatov@yandex.ru>
2026-01-28 19:16:36 +1000

    updating files

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from src.widget import mask_account_card, get_date
+from src.widget import get_date, mask_account_card
 
 
 def main() -> None:
-    account_card = str("Visa Platinum 8990922113665229")
-    date_format = str("2024-03-11T02:26:18.671407")
+    account_card = "Visa Platinum 8990922113665229"
+    date_format = "2024-03-11T02:26:18.671407"
 
     masked_card = mask_account_card(account_card)
     new_date = get_date(date_format)
--- src/widget.py
+++ src/widget.py
@@ -1,6 +1,6 @@
 import re
 
-from src.masks import get_mask_card_number, get_mask_account
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(account_card: str) -> str:
@@ -11,14 +11,12 @@ def mask_account_card(account_card: str) -> str:
 
     if "счет" in account_card.lower():
         letters_count = "".join(re.findall(r"\D+", account_card))
-        numbers_count =  "".join(re.findall(r"\d+", account_card))
+        numbers_count = "".join(re.findall(r"\d+", account_card))
         return f"{letters_count.capitalize()} {get_mask_account(numbers_count)}"
     else:
         letters_card = "".join(re.findall(r"\D+", account_card))
         numbers_card = "".join(re.findall(r"\d+", account_card))
         return f"{letters_card} {get_mask_card_number(numbers_card)}"
-
-
 
 
 def get_date(date_format: str) -> str:
@@ -27,7 +25,7 @@ def get_date(date_format: str) -> str:
      и возвращает строку с датой в формате "ДД.ММ.ГГГГ" ("11.03.2024").
     """
 
-    date_split_list = date_format.split('T')
+    date_split_list = date_format.split("T")
     formated_date = re.sub(r"(\d{4})-(\d{2})-(\d{2})", r"\3.\2.\1", (date_split_list[0]))
 
     return formated_date
