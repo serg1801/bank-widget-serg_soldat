11b8edd7bf0db6e73d1bc04ccecffea9abb9cef9
Soldatov Sergei <serg1801soldatov@yandex.ru>
2026-02-05 10:21:41 +1000

    corrections in processing.py and widget.py

--- src/processing.py
+++ src/processing.py
@@ -2,9 +2,7 @@ from datetime import datetime
 from typing import Any, Dict, List
 
 
-def filter_by_state(
-    transactions: List[Dict[str, Any]], state: str = "EXECUTED"
-) -> List[Dict[str, Any]]:
+def filter_by_state(transactions: List[Dict[str, Any]], state: str = "EXECUTED") -> List[Dict[str, Any]]:
     """
       Принимает список словарей и опционально значение для ключа 'state'(по умолчанию 'EXECUTED'),
      и возвращает новый список словарей, содержащий только те словари,
@@ -14,21 +12,17 @@ def filter_by_state(
     return [data for data in transactions if data.get("state") == state]
 
 
-def sort_by_date(
-    transactions: list[Dict[str, Any]], reverse_order: bool = True
-) -> List[Dict[str, Any]]:
+def sort_by_date(operations: list[Dict[str, Any]], reverse_order: bool = True) -> List[Dict[str, Any]]:
     """
       Принимает список словарей и необязательный параметр, задающий порядок сортировки (по умолчанию — убывание),
     и возвращает новый список, отсортированный по дате (date).
     """
     # Преобразуем строки дат в объекты datetime для сортировки
-    for item in transactions:
+    for item in operations:
         item["date"] = datetime.strptime(item["date"], "%Y-%m-%dT%H:%M:%S.%f")
 
     # Сортируем список словарей по ключу 'date'
-    sorted_list: List[Dict[str, Any]] = sorted(
-        transactions, key=lambda x: x["date"], reverse=reverse_order
-    )
+    sorted_list: List[Dict[str, Any]] = sorted(operations, key=lambda x: x["date"], reverse=reverse_order)
 
     # Преобразуем обратно в строковый формат
     for item in sorted_list:
