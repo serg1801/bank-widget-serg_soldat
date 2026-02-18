64237d791c5accee44cdf03b5f7dca166b9ab7cb
Soldatov Sergei <serg1801soldatov@yandex.ru>
2026-02-18 23:21:20 +1000

    code revision and added to README.md

--- src/generators.py
+++ src/generators.py
@@ -1,31 +1,28 @@
-from typing import Any, Dict, List
+from typing import Any, Dict, Iterator, List
 
 
-def filter_by_currency(transactions: List[Dict[str, Any]], currency_code: str) -> iter:
+def filter_by_currency(transactions: List[Dict[str, Any]], currency_code: str) -> Iterator[Dict[str, Any]]:
     """
-      Принимает на вход список словарей, представляющих транзакции, и  возвращает  итератор,
-     который поочередно выдает транзакции, где валюта операции соответствует заданной (например, USD).
+     Принимает на вход список словарей, представляющих транзакции, и  возвращает  итератор,
+    который поочередно выдает транзакции, где валюта операции соответствует заданной (например, USD).
     """
     for transaction in transactions:
-        if transaction['operationAmount']['currency']['code'] == currency_code:
+        if transaction["operationAmount"]["currency"]["code"] == currency_code:
             yield transaction
 
 
-
-
-def transaction_descriptions(transactions: List[Dict[str, Any]]) -> iter:
+def transaction_descriptions(transactions: List[Dict[str, Any]]) -> Iterator[Dict[str, Any]]:
     """
-      Генератор. Принимает список словарей с транзакциями и возвращает описание каждой операции по очереди.
+    Генератор. Принимает список словарей с транзакциями и возвращает описание каждой операции по очереди.
     """
     for transaction in transactions:
-        yield transaction['description']
+        yield transaction["description"]
 
 
-
-def card_number_generator(start: int, stop: int) -> iter:
+def card_number_generator(start: int, stop: int) -> Iterator[str]:
     """
-      Генератор. Выдает номера банковских карт в формате XXXX XXXX XXXX XXXX, где X— цифра номера карты.
-     Может сгенерировать номера карт в заданном диапазоне от 0000 0000 0000 0001 до 9999 9999 9999 9999.
+     Генератор. Выдает номера банковских карт в формате XXXX XXXX XXXX XXXX, где X— цифра номера карты.
+    Может сгенерировать номера карт в заданном диапазоне от 0000 0000 0000 0001 до 9999 9999 9999 9999.
     """
     for number in range(int(start), int(stop) + 1):
         card_number = str(number).zfill(16)  # заполняем нулями до 16 цифр
--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,15 +1,18 @@
-from typing import Any, Dict, List, Union
+from typing import Any, Dict, List
 
 import pytest
 
-from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
+from src.generators import card_number_generator, filter_by_currency, transaction_descriptions
 
 
-@pytest.mark.parametrize("currency_code, expected_count", [
-    ("USD", 3),  # Три транзакции в USD
-    ("RUB", 2),  # Две транзакции в RUB
-    ("EUR", 0),  # Нет транзакций в EUR
-])
+@pytest.mark.parametrize(
+    "currency_code, expected_count",
+    [
+        ("USD", 3),  # Три транзакции в USD
+        ("RUB", 2),  # Две транзакции в RUB
+        ("EUR", 0),  # Нет транзакций в EUR
+    ],
+)
 def test_filter_by_currency(transactions_: List[Dict[str, Any]], currency_code: str, expected_count: int) -> None:
     result = list(filter_by_currency(transactions_, currency_code))
     assert len(result) == expected_count
@@ -18,29 +21,40 @@ def test_filter_by_currency(transactions_: List[Dict[str, Any]], currency_code: 
     assert len(result) == 0
 
 
-@pytest.mark.parametrize("transactions_, expected_descriptions", [
-    ([  # Список транзакций
-        {'description': 'Перевод организации'},
-        {'description': 'Перевод со счета на счет'},
-        {'description': 'Перевод со счета на счет'},
-        {'description': 'Перевод с карты на карту'},
-        {'description': 'Перевод организации'}
-    ],
+@pytest.mark.parametrize(
+    "transactions_, expected_descriptions",
     [
-        "Перевод организации", "Перевод со счета на счет", "Перевод со счета на счет",
-        "Перевод с карты на карту", "Перевод организации"
-    ]),
-    ([], [])  # Пустой список транзакций
-])
+        (
+            [  # Список транзакций
+                {"description": "Перевод организации"},
+                {"description": "Перевод со счета на счет"},
+                {"description": "Перевод со счета на счет"},
+                {"description": "Перевод с карты на карту"},
+                {"description": "Перевод организации"},
+            ],
+            [
+                "Перевод организации",
+                "Перевод со счета на счет",
+                "Перевод со счета на счет",
+                "Перевод с карты на карту",
+                "Перевод организации",
+            ],
+        ),
+        ([], []),  # Пустой список транзакций
+    ],
+)
 def test_transaction_descriptions(transactions_: List[Dict[str, Any]], expected_descriptions: List[str]) -> None:
     result = list(transaction_descriptions(transactions_))
     assert result == expected_descriptions
 
 
-@pytest.mark.parametrize("start, stop, expected", [
-    (1, 2, ["0000 0000 0000 0001", "0000 0000 0000 0002"]),
-    (5, 5, ["0000 0000 0000 0005"]),
-])
-def test_card_number_generator(start, stop, expected) -> None:
+@pytest.mark.parametrize(
+    "start, stop, expected",
+    [
+        (1, 2, ["0000 0000 0000 0001", "0000 0000 0000 0002"]),
+        (5, 5, ["0000 0000 0000 0005"]),
+    ],
+)
+def test_card_number_generator(start: int, stop: int, expected: List[str]) -> None:
     result = list(card_number_generator(start, stop))
     assert result == expected
